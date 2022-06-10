ab7a75fecce5124dc28ebdb6c99437180b4d21b6
Bart-Zw <90245778+Bart-Zw@users.noreply.github.com>
2022-06-10 13:03:00 +0200

    peer review

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
-
+// al je javascript in de main doen is wat makkelijker als je gaat nakijke
 canvas.width = innerWidth
 canvas.height = innerHeight
